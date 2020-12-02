6f651b20569fedbfb74c9019f6662c362b495a8e
Людмила <lm8023740@gmail.com>
2020-12-02 19:55:03 +0300

    добавленыстили

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let btnDefault= document.querySelector('.btn-default');
+let intro=document.querySelector('.intro');
+
+btnDefault.addEventListener('click',()=>{alert('Щелчок по кнопке');});
+intro.style= "5px dashed gold";
